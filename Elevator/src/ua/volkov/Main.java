ab528bcd0f70d682f8b43cf2448d3dd2b8d93a4a
Oleksandr Makarevich <oleksandr.makarevich@oracle.com>
2022-07-15 12:05:53 +0300

    initial commit

---
+++ Elevator/src/ua/volkov/Main.java
@@ -0,0 +1,16 @@
+package ua.volkov;
+import ua.volkov.model.Building;
+
+import java.util.Random;
+
+public class Main {
+    private static Random r = new Random();
+
+    public static void main(String[] args) {
+
+       Building building = new Building((r.nextInt(16)+5)); // (0...15) +5 = 5... 20
+        System.out.println("START");
+        System.out.println(building);
+       building.startCycle();
+    }
+}
---
+++ Elevator/src/ua/volkov/interfaces/IElevator.java
@@ -0,0 +1,14 @@
+package ua.volkov.interfaces;
+
+public interface IElevator {
+
+    int move();
+    boolean isEmpty();
+    boolean isFull();
+    void correctDirrection();
+    boolean isDirection();
+    int getCurrentFloor();
+    void addPassenger(int passengerFloor);
+    int removePassengers();
+    void setDirection(boolean direction);
+}
